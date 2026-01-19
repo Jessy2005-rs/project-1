7a3b172d6d60d16dca770a5622e3b5237ab8953d
Jessy2005-rs <jessychrista2005@gmail.com>
2026-01-19 14:48:51 +0530

    Add files via upload

---
+++ healthy-life-guide/script.js
@@ -0,0 +1,19 @@
+function showFoodTip() {
+    document.getElementById("foodTip").innerHTML =
+        "Eat more fruits and vegetables for better immunity.";
+}
+
+function showExerciseTip() {
+    document.getElementById("exerciseTip").innerHTML =
+        "30 minutes of walking daily keeps your heart healthy.";
+}
+
+function showWaterTip() {
+    document.getElementById("waterTip").innerHTML =
+        "Drink water regularly to keep your body hydrated.";
+}
+
+function showMentalTip() {
+    document.getElementById("mentalTip").innerHTML =
+        "Pray, meditate, and sleep well to reduce stress.";
+}
